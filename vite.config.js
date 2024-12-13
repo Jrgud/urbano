b5eca3f860382b53d7333f611be2bb5a30acd1b3
Jrgud <158764333+Jrgud@users.noreply.github.com>
2024-12-13 07:11:30 -0500

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    outDir: 'build', // Output directory for the built application
+  },
+  plugins: [react()],
+  
+})
